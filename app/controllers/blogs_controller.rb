c00639aaf0caecf714d0d27c83b0424da9be32c5
加瀬徹 <kasetooru@kasetoorunoMacBook.local>
2018-10-10 15:21:54 +0900

    rails assets:precompile RAILS_ENV=production

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,7 +2,7 @@ class BlogsController < ApplicationController
   before_action :set_blog, only:[:show, :edit, :update, :destroy]
 
    def index
-       @blogs = Blog.all
+       @blog = Blog.all
    end
 
    def new
