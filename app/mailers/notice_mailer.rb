4c4dad6aa5613678b1507d33cf34b30765249e4e
加瀬徹 <kasetooru@kasetoorunoMacBook.local>
2018-10-14 11:43:46 +0900

    modify sender

--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -1,7 +1,7 @@
 class NoticeMailer < ApplicationMailer
   def notice_mail(blog)
      @blog = blog
-     @email = current_user.email
-     mail to: @email, subject: "ブログの確認メール"
+     @user = @blog.user
+     mail to: @user, subject: "ブログの確認メール"
   end
 end
